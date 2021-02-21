96c370b40093280f6452364ede65642fca089ff6
soushiki <79419348+soushiki@users.noreply.github.com>
2021-02-21 14:50:22 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -4,8 +4,8 @@ module.exports = {
         connectionLimit: 60,
         host: procces.env.DB_host || 'localhost',
         user: process.env.DB_USER || 'root',
-        password: '',
-        database: 'db_registro'
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DATABASE || 'db_registro'
     }
 
 };
