d32ca7509efb85c9700241fa1d6da198e76be624
soushiki <79419348+soushiki@users.noreply.github.com>
2021-02-21 21:08:45 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const { promisify }= require('util');
 
 const { database } = require('./keys');
 
-const pool = mysql.createPool(process.env.JAWSDB_MARIA_URL);
+const pool = mysql.createPool(database);
 
 
 pool.getConnection((err, connection) => {
