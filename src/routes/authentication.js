f180a56355b356574f31e9375b6c732b220f10cb
soushiki <79419348+soushiki@users.noreply.github.com>
2021-02-21 21:54:41 -0500

    Update authentication.js

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -21,8 +21,8 @@ router.get('/signin',  (req, res) => {
 });
 
 router.post('/signin', (req, res, next) => {
-  req.check('username', 'Username is Required').notEmpty();
-  req.check('password', 'Password is Required').notEmpty();
+  req.check('username', 'Username is Required').not().isEmpty();
+  req.check('password', 'Password is Required').not().isEmpty();
   const errors = req.validationErrors();
   if (errors.length > 0) {
     req.flash('message', errors[0].msg);
