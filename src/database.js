b6d84c2d53191a99861536486a298e83c7e7b8c9
soushiki <79419348+soushiki@users.noreply.github.com>
2021-02-21 21:07:00 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,20 +1,10 @@
 const mysql = require('mysql');
-const connection = mysql.createConnection(process.env.JAWSDB_URL);
 const { promisify }= require('util');
 
 const { database } = require('./keys');
 
-const pool = mysql.createPool(database);
+const pool = mysql.createPool(process.env.JAWSDB_MARIA_URL);
 
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
 
 pool.getConnection((err, connection) => {
   if (err) {
