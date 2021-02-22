343ee24e57cf195aef6b7092eae37209ae98df4e
soushiki <79419348+soushiki@users.noreply.github.com>
2021-02-21 20:41:33 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -7,7 +7,7 @@ module.exports = {
           acquireTimeout: 60 * 60 * 1000,
           timeout: 60 * 60 * 1000,
         
-        host: process.env.DB_host || 'localhost',
+        host: process.env.DB_HOST || 'localhost',
         user: process.env.DB_USER || 'root',
         password: process.env.DB_PASSWORD || '',
         database: process.env.DB_DATABASE || 'db_registro',
